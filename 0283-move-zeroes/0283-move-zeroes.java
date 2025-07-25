046f8591c175ad73b2f25c6a139fba16b72cf303
ASHWIKA1 <168112126+ASHWIKA1@users.noreply.github.com>
2025-07-25 21:36:18 +0530

    Time: 2 ms (80.27%), Space: 46.3 MB (44.28%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-int count=0;
-      for(int i=0;i<nums.length;i++){
-       
-      if(nums[i]!=0){
-        nums[count++]=nums[i];
-      }
-      }
-      while(count<nums.length){
-        nums[count++]=0;
-      }
-        
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+                 
+            if(nums[j]!=0){
+
+            int t=nums[j];
+            nums[j]=nums[i];
+            nums[i]=t;
+                  i++;
+            }
+                  
+        }
     }
+    
 }
