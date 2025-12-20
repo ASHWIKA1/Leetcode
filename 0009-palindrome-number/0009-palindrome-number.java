03140735c7973d89e29a455836be9e38e0c4874a
ASHWIKA1 <168112126+ASHWIKA1@users.noreply.github.com>
2025-12-20 17:26:30 +0530

    Time: 5 ms (73.94%), Space: 46.2 MB (9.27%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,27 @@
 class Solution {
     public boolean isPalindrome(int x) {
+                if (x > Integer.MAX_VALUE || x < Integer.MIN_VALUE) {
+            return false;
+        }
+        long y=0;
         if(x<0){
             return false;
         }
-        int rev = 0;
-        int  num= x;
-
-        while (num!= 0) {
-            rev= rev*10 + num%10;
-            num=num/10;
+         
+        else{
+        long quo=x;
+       
+            
+            while(quo>0){
+            long last=quo%10;
+             y=y*10+last;
+             quo=quo/10;
+            }
         }
-
-        return (rev == x);
+        if(x==(int)y){
+            return true;
+            }
+            return false;
     }
+    
 }
